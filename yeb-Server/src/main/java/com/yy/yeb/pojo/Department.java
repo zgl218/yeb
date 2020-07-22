c4ae7fdc8ef6df46357a50ad96f118911b79b3a4
zgl <401976209@qq.com>
2020-07-22 19:37:40 +0800

    zgl_部门初始

--- yeb-Server/src/main/java/com/yy/yeb/pojo/Department.java
+++ yeb-Server/src/main/java/com/yy/yeb/pojo/Department.java
@@ -1,9 +1,12 @@
 package com.yy.yeb.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+import java.util.List;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -45,6 +48,10 @@ public class Department implements Serializable {
 
     @ApiModelProperty(value = "是否上级")
     private Boolean isParent;
+
+    @ApiModelProperty(value = "子节点")
+    @TableField(exist = false)
+    private List<Department> children;
 
 
 }
