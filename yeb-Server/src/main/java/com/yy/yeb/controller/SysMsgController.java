05fe430682d9a793ac917e1298eb62fdf2ef5b89
zgl <401976209@qq.com>
2020-07-23 09:50:48 +0800

    zgl_部门初始添加

--- yeb-Server/src/main/java/com/yy/yeb/controller/SysMsgController.java
+++ yeb-Server/src/main/java/com/yy/yeb/controller/SysMsgController.java
@@ -1,13 +1,9 @@
 package com.yy.yeb.controller;
 
 
-import com.yy.yeb.service.WebSocketServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * <p>
@@ -18,14 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @since 2020-07-17
  */
 @RestController
-//@RequestMapping("/sys-msg")
+@RequestMapping("/sys-msg")
 public class SysMsgController {
-    @Autowired
-    private WebSocketServer webSocketServer;
 
-    @GetMapping("/ws/chat")
-    public ModelAndView socket(){
-        ModelAndView mav = new ModelAndView("/ws/chat");
-        return mav;
-    }
 }
